ba691208a95dcaeca5025765d9546cf6b188b586
Pallavi Reddy <pallavireddy13166@gmail.com>
2025-12-13 19:51:17 +0530

    fix code error

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -7,14 +7,16 @@ const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",    
-      port: 465,                 
-      secure: true,             
+      port: 587,                 
+      secure: false,             
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS, 
       },
      
-      family: 4, 
+      family: 4,
+      logger: true,
+      debug: true 
     });
 
     
