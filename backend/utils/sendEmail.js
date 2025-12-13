8f3f90523e5402d79c4ec30053d3e8a8aa0bbf43
Pallavi Reddy <pallavireddy13166@gmail.com>
2025-12-13 21:31:12 +0530

    fix error

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,49 +1,22 @@
 // backend/utils/sendEmail.js
-const nodemailer = require("nodemailer");
+const { Resend } = require('resend');
 
-const sendEmail = async (to, subject, text) => {
-  console.log(`Attempting to send email to: ${to}`); 
+const resend = new Resend(process.env.re_aW7SzJ7y_9qVH2wzUKAfzRgq53cyAuTje);
 
+const sendEmail = async (to, subject, htmlContent) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",    
-      port: 587,                 
-      secure: false,             
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, 
-      },
-     
-      family: 4,
-      logger: true,
-      debug: true 
+    const data = await resend.emails.send({
+      from: 'onboarding@resend.dev', 
+      to: 'pallavireddy13166@gmail.com',
+      subject: 'Hello World',
+  html: '<p>Congrats on sending your first email!</p>', 
     });
 
-    
-    await new Promise((resolve, reject) => {
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log("Transporter verification failed:", error);
-          reject(error);
-        } else {
-          console.log("Server is ready to take our messages");
-          resolve(success);
-        }
-      });
-    });
-
-    const info = await transporter.sendMail({
-      from: `"EduFeedback Admin" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text,
-    });
-
-    console.log(`Email sent successfully! Message ID: ${info.messageId}`); 
-
+    console.log("Email sent successfully via Resend:", data);
+    return data;
   } catch (error) {
-    console.error("!!! Email Sending Error Caught !!!:", error); 
-    throw new Error(`Failed to send email: ${error.message}`); 
+    console.error("Resend Error:", error);
+    throw new Error(error.message);
   }
 };
 
