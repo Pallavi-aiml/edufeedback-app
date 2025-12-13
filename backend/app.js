f5806e399369090f9afb1bb349ab6f91c3dddc3e
Pallavi Reddy <pallavireddy13166@gmail.com>
2025-12-13 23:33:51 +0530

    Configured for live Vercel testing

--- backend/app.js
+++ backend/app.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 if (process.env.NODE_ENV !== 'production') {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => { 
-      console.log(`🚀 Server running locally at http://localhost:${PORT}`);
+      console.log(`Server running locally at http://localhost:${PORT}`);
     });
 }
 
